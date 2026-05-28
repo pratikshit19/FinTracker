7978791a29c34e7145a53f77a96e93f87a261992
pratikshit19 <pratikshit@icloud.com>
2026-05-28 20:27:58 +0530

    corrected pswd reset

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-a90db6fa'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.ljdhq8l242g"
+    "revision": "0.ukc1koqi2a"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
